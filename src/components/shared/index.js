2793e8987cbdc68323ec011addf1e349d888e64c
Callum Bonnyman <callum@wrux.com>
2023-09-14 13:57:48 +0900

    Various layout and styling adjustments.

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -1,3 +1,4 @@
 export { default as Credits } from './Credits.astro';
 export { default as Heading } from './Heading.astro';
 export { default as PostTeaser } from './PostTeaser.astro';
+export { default as Text } from './Text.astro';
