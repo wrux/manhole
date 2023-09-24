95297b13531fa66b688326be3e1c19c2a3f12ce2
Callum Bonnyman <callum@wrux.com>
2023-09-24 15:48:05 +0900

    Updated post list view subtitle in Sanity Studio.

--- src/sanity/schemas/documents/post.ts
+++ src/sanity/schemas/documents/post.ts
@@ -174,18 +174,16 @@ export default defineType({
   ],
   preview: {
     select: {
+      _createdAt: '_createdAt',
       title: 'title',
-      author: 'author.name',
-      date: 'date',
       media: 'mainImage',
     },
-    prepare({ title, media, author, date }) {
-      const subtitles = [
-        author && `by ${author}`,
-        date && `on ${format(parseISO(date), 'LLL d, yyyy')}`,
-      ].filter(Boolean);
-
-      return { title, media, subtitle: subtitles.join(' ') };
+    prepare({ _createdAt, title, media }) {
+      return {
+        title,
+        media,
+        subtitle: `Posted on ${format(parseISO(_createdAt), 'LLL d, yyyy')}`,
+      };
     },
   },
 });
