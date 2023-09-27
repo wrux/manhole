efa6192e911b3572a797c7bebcf9fb865f734898
Callum Bonnyman <callum@wrux.com>
2023-09-27 15:27:59 +0900

    Added Manhole Gallery wordmark.

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Heading } from './Heading.astro';
 export { default as PortableText } from './PortableText/PortableText.astro';
 export { default as PostTeaser } from './PostTeaser.astro';
 export { default as Text } from './Text.astro';
+export { default as Wordmark } from './Wordmark.astro';
