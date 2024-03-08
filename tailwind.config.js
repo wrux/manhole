7098d8daab70dccef3151eabc948274dcee063ec
Callum Bonnyman <callum@wrux.com>
2024-03-08 22:05:41 +0700

    Added Tailwind 4.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,11 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      },
-    },
     extend: {
-      // https://colorhunt.co/palette/451952662549ae445af39f5a
-      colors: {
-        primary: { DEFAULT: '#451952', alt: '#662549' },
-        secondary: '#AE445A',
-        tertiary: '#F39F5A',
-      },
-      dropShadow: {
-        heavy: '2px 4px 6px rgba(0, 0, 0, 0.666)',
-      },
-      fontFamily: {
-        sans: ['InterVariable', 'Inter', ...defaultTheme.fontFamily.sans],
-      },
       screens: {
-        xs: '475px',
+        xs: "475px",
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require("@tailwindcss/typography")],
 };
