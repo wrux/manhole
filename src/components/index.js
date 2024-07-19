dfaa69a17d93c9b0fb82089c63f011f105343f7b
Callum Bonnyman <callum@wrux.com>
2024-07-19 08:43:35 +0100

    Fixed card image view transition stutters.

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { default as CountryShape } from './CountryShape.astro';
 export { default as Credits } from './Credits.astro';
 export { default as Heading } from './Heading.astro';
 export { default as PortableText } from './PortableText/PortableText.astro';
+export { default as ManholeImage } from './ManholeImage.astro';
 export { default as PostTeaser } from './PostTeaser.astro';
 export { default as Text } from './Text.astro';
 export { default as Wordmark } from './Wordmark.astro';
